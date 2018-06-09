188dbb51039cfd2b6c50dd46981a055694cc0ce1
Miyuu Okabe <meetan625@gmail.com>
2018-06-09 11:10:12 +0900

    add page

---
+++ app/src/main/java/com/miyuu/android/jirotra/InputActivity.java
@@ -0,0 +1,13 @@
+package com.miyuu.android.jirotra;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class InputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}
