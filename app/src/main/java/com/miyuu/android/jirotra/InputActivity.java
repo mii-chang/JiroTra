7633e9a594f9730b788e22c0b765e4240ee9afd4
Miyuu Okabe <meetan625@gmail.com>
2018-06-09 13:27:21 +0900

    add Listview to dialog

--- app/src/main/java/com/miyuu/android/jirotra/InputActivity.java
+++ app/src/main/java/com/miyuu/android/jirotra/InputActivity.java
@@ -5,17 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.ArrayList;
-
 public class InputActivity extends AppCompatActivity implements InputDialogFragment.OnDialogButtonClickListener {
 
     int[] buttonId = {R.id.menButton, R.id.yasaiButton, R.id.aburaButton, R.id.tareButton, R.id.garlicButton};
-    String[] titles = {"麺の量を選択", "野菜の量を選択", "アブラの量を選択", "カラメを選択", "にんにくの量を選択"};
-    String[] messages = {"麺の量を選択", "野菜の量を選択", "アブラの量を選択", "カラメを選択", "にんにくの量を選択"};
-    String[] masiCall = {"なし", "少なめ", "普通", "マシ", "マシマシ"};
-    String[] menCall = {"普通", "少なめ", "半分", "多め", "マシマシ"};
-    ArrayList<String> masiList = new ArrayList<>();
-    ArrayList<String> menList = new ArrayList<>();
+    final String[] titles = {"麺の量を選択", "野菜の量を選択", "アブラの量を選択", "カラメを選択", "にんにくの量を選択"};
+    final String[] messages = {"麺の量を選択", "野菜の量を選択", "アブラの量を選択", "カラメを選択", "にんにくの量を選択"};
+    final String[] masiCall = {"なし", "少なめ", "普通", "マシ", "マシマシ"};
+    final String[] menCall = {"普通", "少なめ", "半分", "多め", "マシマシ"};
     Button[] buttons = new Button[5];
 
     @Override
@@ -23,19 +19,9 @@ public class InputActivity extends AppCompatActivity implements InputDialogFragm
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
-        for (int i = 0; i < masiCall.length; i++) {
-            masiList.add(masiCall[i]);
-        }
-        for (int i = 0; i < menCall.length; i++) {
-            menList.add(menCall[i]);
-        }
-
-
         for (int i = 0; i < buttonId.length; i++) {
             final String title = titles[i];
             final String message = messages[i];
-            final ArrayList<String> callMenList = menList;
-            final ArrayList<String> callMasiList = masiList;;
             final int callType = i;
 
 
@@ -43,7 +29,7 @@ public class InputActivity extends AppCompatActivity implements InputDialogFragm
             buttons[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    InputDialogFragment fragment = InputDialogFragment.newInstance(title, message,callMenList, callMasiList,callType);
+                    InputDialogFragment fragment = InputDialogFragment.newInstance(title, message,menCall, masiCall,callType);
                     fragment.show(getSupportFragmentManager(), "input_dialog");
                 }
             });
--- app/src/main/java/com/miyuu/android/jirotra/InputDialogFragment.java
+++ app/src/main/java/com/miyuu/android/jirotra/InputDialogFragment.java
@@ -2,29 +2,33 @@ package com.miyuu.android.jirotra;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.support.v4.app.DialogFragment;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
-
-import java.util.ArrayList;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class InputDialogFragment extends DialogFragment {
+    AlertDialog dialog;
+    AlertDialog.Builder builder;
 
     public interface OnDialogButtonClickListener {
         void onPositiveClick(String item);
+
         void onNegativeClick();
     }
 
     private OnDialogButtonClickListener mListener;
 
-    public static InputDialogFragment newInstance(String title, String message, ArrayList menList,ArrayList masiList,int callType) {
+    public static InputDialogFragment newInstance(String title, String message, String[] menList, String[] masiList, int callType) {
         InputDialogFragment fragment = new InputDialogFragment();
         Bundle bundle = new Bundle();
         bundle.putString("title", title);
         bundle.putString("message", message);
-        bundle.putStringArrayList("menList", menList);
-        bundle.putStringArrayList("masiList", masiList);
+        bundle.putStringArray("menList", menList);
+        bundle.putStringArray("masiList", masiList);
         bundle.putInt("callType", callType);
         fragment.setArguments(bundle);
         return fragment;
@@ -38,32 +42,53 @@ public class InputDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
         Bundle args = getArguments();
         String title = args.getString("title");
         String message = args.getString("message");
-        ArrayList menList = args.getStringArrayList("menList");
-        ArrayList masiList = args.getStringArrayList("masiList");
+        String[] callList;
         int callType = args.getInt("callType");
 
+        if (callType == 0) {
+            //麺
+            callList = args.getStringArray("menList");
+        } else {
+            callList = args.getStringArray("masiList");
+        }
+        View dialogView = getActivity().getLayoutInflater().inflate(R.layout.dialog_input, null);
+        Button positiveButton = dialogView.findViewById(R.id.positive_button);
+        Button negativeButton = dialogView.findViewById(R.id.negative_button);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        mListener.onPositiveClick("a");
-                        dismiss();
-                    }
-                })
-                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dismiss();
-                        mListener.onNegativeClick();
-                        dismiss();
-                    }
-                });
-        return builder.create();
+        ListView listView = dialogView.findViewById(R.id.listVIew);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1, callList);
+        listView.setAdapter(adapter);
+
+        builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title);
+        builder.setMessage(message);
+
+        positiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Listから選んだItemをListenerにわたす
+//                mListener.onPositiveClick(totalPrice);
+                dismiss();
+            }
+        });
+
+        negativeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+                mListener.onNegativeClick();
+                dismiss();
+            }
+        });
+
+        builder.setView(dialogView);
+
+        dialog = builder.create();
+        dialog.show();
+        return dialog;
     }
 }
